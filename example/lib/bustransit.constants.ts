6adfc792ca142622a4dc97e9c859eec92a1fa55f
nguyen phuc nguyen <>
2025-12-22 18:53:28 +0700

    no message

--- example/lib/bustransit.constants.ts
+++
@@ -1,4 +0,0 @@
-export const BUSTRANSIT_MODULE_OPTIONS = 'BusTransitModuleOptions';
-export const BUSTRANSIT_CONSUMERS = 'BusTransitConsumers';
-export const BUSTRANSIT_CONSUMERS_BIND_QUEUE = 'BusTransitConsumersBindQueue';
-export const BUSTRANSIT_MESSSAGES_BIND_QUEUE = 'BusTransitMessagesBindQueue'
